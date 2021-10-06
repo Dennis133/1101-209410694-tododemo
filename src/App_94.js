2f24d6cd383a24eb22dc444edd6baf68b50df8a9
Dennis133 <209410694@gms.tku.edu.tw>
2021-10-06 12:16:31 +0800

    first commit

---
+++ src/App_94.js
@@ -0,0 +1,15 @@
+import Todo_94 from './components/Todo_94';
+import './App.css';
+
+function App_94() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo_94 text='Learn Java' />
+      <Todo_94 text='Learn Python' />
+      <Todo_94 text='Learn MongoDB' />
+    </div>
+  );
+}
+
+export default App_94;
