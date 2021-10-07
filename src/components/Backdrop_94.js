5cc8b3d2cd8b75cc038353dae03a28c757443313
Dennis133 <209410694@gms.tku.edu.tw>
2021-10-07 19:51:48 +0800

    add backdrop,modal

---
+++ src/components/Backdrop_94.js
@@ -0,0 +1,5 @@
+function Backdrop_94(props) {
+  return <div className='backdrop' onClick={props.onClose}></div>;
+}
+
+export default Backdrop_94;
--- src/components/Todo_94.js
+++ src/components/Todo_94.js
@@ -1,12 +1,31 @@
+import { useState } from 'react';
+import Modal_94 from '../components/Modal_94';
+import Backdrop_94 from '../components/Backdrop_94';
+
 function Todo_94(props) {
+  const [showModal, setShowModal] = useState();
+
+  function showModalHandler() {
+    setShowModal(true);
+  }
+
+  function closeModalHandler() {
+    setShowModal(false);
+  }
   return (
     <div>
       <div className='card'>
         <h2>{props.text}</h2>
         <div className='actions'>
-          <button className='btn'>Delete</button>
+          <button className='btn' onClick={showModalHandler}>
+            Delete
+          </button>
         </div>
       </div>
+      {showModal && <Backdrop_94 onClose={closeModalHandler} />}
+      {showModal && (
+        <Modal_94 text='Are you sure?' onClose={closeModalHandler} />
+      )}
     </div>
   );
 }
